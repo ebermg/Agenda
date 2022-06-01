900c9f44dc5ee1c0795031058c57b4fdcff38bb1
ebermg <69368075+ebermg@users.noreply.github.com>
2022-06-01 14:54:07 -0500

    Agregar archivos de proyecto.

---
+++ Agenda.iOS/SQLiteDB.cs
@@ -0,0 +1,27 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+using SQLite;
+using Xamarin.Forms;
+using System.IO;
+using Agenda.Datos;
+using Agenda.iOS;
+
+[assembly: Dependency(typeof(SQLiteDB))]
+namespace Agenda.iOS
+{
+    public class SQLiteDB : ISQLiteDB
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var ruta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            //Creacion de la base de datos
+            var path = Path.Combine(ruta, "AgendaSQLite.db3");
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
